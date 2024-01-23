eab6c0096bd9d181d5b5c9caabd1a58c55f26c46
profAndreaFranca <dejandreafranca@gmail.com>
2024-01-22 21:16:39 -0300

    stack navigator

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Home from './screens/home';
 import Updates from './screens/updates';
 import Meteors from './screens/meteors';
 import ISSlocation from './screens/ISSLocation';
+import StackNavigator from './navigation/stackNavigator';
 
 export default function App() {
   return (
---
+++ navigation/stackNavigator.js
@@ -0,0 +1,18 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../screens/home';
+import Updates from '../screens/updates';
+import Meteors from '../screens/meteors';
+import ISSlocation from '../screens/ISSLocation';
+
+const Stack = createStackNavigator();
+
+export default function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Meteors" component={Meteors} />
+      <Stack.Screen name="ISSLocation" component={ISSlocation} />
+      <Stack.Screen name="Updates" component={Updates} />
+    </Stack.Navigator>
+  );
+}
