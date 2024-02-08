05f001d500166fd575c8cbc99ff537ee2f2a34f1
profAndreaFranca <dejandreafranca@gmail.com>
2024-02-07 21:20:17 -0300

    coleta dos meteros
    Co-authored-by: player1ytoficial@gmail.com <player1ytoficial@gmail.com>
    Co-authored-by: pindread <iarobotico@users.noreply.github.com>
    Co-authored-by: JPSouza12 <JPSouza12@users.noreply.github.com>
    Co-authored-by: AlexandreGamboa <AlexandreGamboa@users.noreply.github.com>
    Co-authored-by: Naoeofrancesco <Naoeofrancesco@users.noreply.github.com>

--- screens/ISSLocation.js
+++ screens/ISSLocation.js
@@ -1,4 +1,4 @@
-import { axios } from "axios";
+import axios from "axios";
 import React from "react";
 import {
   Text,
@@ -10,9 +10,8 @@ import {
   ImageBackground,
   Image,
   Alert,
-  TouchableOpacity,
 } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+// import { TouchableOpacity } from "react-native-gesture-handler";
 import { MapView, Marker } from "react-native-maps";
 
 var bg = require("../assets/iss_bg.jpg");
@@ -26,49 +25,72 @@ export default class ISSlocation extends React.Component {
     };
   }
 
-  componentDidMount(){
-    this.getISSLocation()
+  componentDidMount() {
+    this.getISSLocation();
   }
 
-  getISSLocation =()=>{
+  getISSLocation = () => {
     axios
-        .get("https://api.wheretheiss.at/v1/satellites/25544")
-        .then(response =>{this.setState({location:response.data})})
-        .catch(error =>Alert.alert(error.message))
-  }
+      .get("https://api.wheretheiss.at/v1/satellites/25544")
+      .then((response) => {
+        this.setState({ location: response.data });
+        // console.log(response)
+      })
+      .catch((error) => Alert.alert(error.message));
+  };
   render() {
-    return (
-      <View style={styles.container}>
-        <SafeAreaView style={styles.droidSafeArea}/>
-        <ImageBackground source ={bg} style={styles.backgroundImage}>
+    if (Object.keys(this.state.location).length === 0) {
+      return (
+        <View style={styles.loading}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <ImageBackground source={bg} style={styles.backgroundImage}>
             <View style={styles.titleContainer}>
-                <Text style={styles.titleText}> ISS Location </Text>
+              <Text style={styles.titleText}> ISS Location </Text>
             </View>
             <View style={styles.mapContainer}>
-                <MapView style={styles.map} 
+              <MapView
+                style={styles.map}
                 region={{
+                  latitude: this.state.location.latitude,
+                  longitude: this.state.location.longitude,
+                  latitudeDelta: 100,
+                  longitudeDelta: 100,
+                }}
+              >
+                <Marker
+                  coordinate={{
                     latitude: this.state.location.latitude,
                     longitude: this.state.location.longitude,
-                    latitudeDelta: 100,
-                    longitudeDelta: 100,
-                }}>
-                    <Marker coordinate={{
-                        latitude: this.state.location.latitude,
-                        longitude: this.state.location.longitude,
-                    }}>
-                        <Image style={{width:50, height:50}} source={issIcon}/>
-                    </Marker>
-                </MapView>
+                  }}
+                >
+                  <Image style={{ width: 50, height: 50 }} source={issIcon} />
+                </Marker>
+              </MapView>
             </View>
             <View style={styles.infoContainer}>
-                <Text style={styles.infoText}> Latitude:{this.state.location.latitude}</Text>
-                <Text style={styles.infoText}> Longitude:{this.state.location.longitude}</Text>
-                <Text style={styles.infoText}> Altitude:{this.state.location.altitude}</Text>
-                <Text style={styles.infoText}> Velocity:{this.state.location.velocity}</Text>
+              <Text style={styles.infoText}>
+                Latitude:{this.state.location.latitude}
+              </Text>
+              <Text style={styles.infoText}>
+                Longitude:{this.state.location.longitude}
+              </Text>
+              <Text style={styles.infoText}>
+                Altitude:{this.state.location.altitude}
+              </Text>
+              <Text style={styles.infoText}>
+                Velocity:{this.state.location.velocity}
+              </Text>
             </View>
-        </ImageBackground>
-      </View>
-    );
+          </ImageBackground>
+        </View>
+      );
+    }
   }
 }
 
@@ -98,23 +120,28 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  mapContainer:{
-    flex:0.6
+  mapContainer: {
+    flex: 0.6,
   },
-  map:{
-    width:"100%",
-    height:'100%'
+  map: {
+    width: "100%",
+    height: "100%",
   },
-  infoContainer:{
-    flex:0.2,
-    backgroundColor:"#fff",
-    marginTop:-10,
-    borderRadius:30,
-    padding:30
+  infoContainer: {
+    flex: 0.2,
+    backgroundColor: "#fff",
+    marginTop: -10,
+    borderRadius: 30,
+    padding: 30,
   },
-  infoText:{
-    fontSize:15,
-    color:"#000",
-    fontWeight:"bold"
-  }
+  infoText: {
+    fontSize: 15,
+    color: "#000",
+    fontWeight: "bold",
+  },
+  loading: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
--- screens/meteors.js
+++ screens/meteors.js
@@ -1,20 +1,44 @@
-import React from "react"
-import {Text, View,StyleSheet} from "react-native"
+import axios from "axios";
+import React from "react";
+import { Text, View, StyleSheet, Alert } from "react-native";
 
-export default class Meteors extends React.Component{    
-    render(){
-        return(
-            <View style={styles.container}>
-                <Text> Meteors </Text>
-            </View>
-        )
-    }
+export default class Meteors extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meteors: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getMeteors();
+  }
+
+  getMeteors = () => {
+    axios
+      .get(
+        "https://api.nasa.gov/neo/rest/v1/feed?api_key=UEG79o7VuN1AvC2nm6N7ze0clLmLOUaAHrKej06i"
+      )
+      .then((response) => {
+        this.setState({ meteors: response.data.near_earth_objects });
+        console.log(Object.keys(this.state.meteors))
+      })
+      .catch((error) => Alert.alert(error.message))
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> Meteors </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center"
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
