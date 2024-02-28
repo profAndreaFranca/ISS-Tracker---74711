048fe4e782aec9d97e1bfb26b444229fe61e5c85
profAndreaFranca <dejandreafranca@gmail.com>
2024-02-28 20:58:58 -0300

    Pontuação de risco
    Co-authored-by: pindread <iarobotico@users.noreply.github.com>
    Co-authored-by: Naoeofrancesco <Naoeofrancesco@users.noreply.github.com>
    Co-authored-by: AlexandreGamboa <AlexandreGamboa@users.noreply.github.com>
    Co-authored-by: JPSouza12 <JPSouza12@users.noreply.github.com>
    Co-authored-by: player1ytoficial@gmail.com <player1ytoficial@gmail.com>

--- screens/meteors.js
+++ screens/meteors.js
@@ -1,3 +1,4 @@
+
 import axios from "axios";
 import React from "react";
 import { Text, View, StyleSheet, Alert } from "react-native";
@@ -27,11 +28,51 @@ export default class Meteors extends React.Component {
   };
 
   render() {
+    //criar uma condição para exibir carregando até que os dados estejam disponíveis - alexandre
+    if(Object.keys(this.state.meteors).length == 0){
+      //no if => exibir carregando na tela - Francesco
+      return(
+        <View style ={styles.container}> 
+          <Text>carregando...</Text>
+        </View>
+      
+      )
+    }else{
+    //mapear os meteoros - João
+    var meteorArray = Object.keys(this.state.meteors).map(meteorDate=>{
+      return this.state.meteors[meteorDate]
+    })
+    var meteors = [].concat.apply([],meteorArray)
+    //pontuação de risco = diamentro/distancia ao passar pela terra
+    //calcular a pontuação de risco - Francisco
+    meteors.forEach(function(element){
+      var diametro = (element.estimated_diameter.kilometers.estimated_diameter_min + 
+      element.estimated_diameter.kilometers.estimated_diameter_max) / 2 
+      var pRisco = (diametro / element.close_approach_data[0].miss_distance.kilometers) * 1000000000 
+      element.pontuacao_Risco = pRisco
+    })
+    //ordenar meteoros conforme pontuação de risco - Iago
+    
+    meteors.sort(function(A,B){
+      return B.pontuacaoRisco - A.pontuacaoRisco
+    })
+    meteors = meteors.slice(0,5)
+    
+    
     return (
       <View style={styles.container}>
         <Text> Meteors </Text>
       </View>
     );
+    }
+    
+
+    
+
+ 
+
+
+    
   }
 }
 
